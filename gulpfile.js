028937dd166e3c4e90798fb2f75bf423656d8675
aydevelop <ay.develop1@gmail.com>
2021-05-07 23:32:59 +0300

    inspiration

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,8 @@ function scripts() {
   return src(
   ['node_modules/jquery/dist/jquery.js', 
   'node_modules/slick-carousel/slick/slick.js',
-  'node_modules/mixitup/dist/mixitup.js',  
+  'node_modules/mixitup/dist/mixitup.js', 
+  'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',  
   'app/js/main.js'])
     .pipe(uglify())
     .pipe(concat('main.min.js'))
